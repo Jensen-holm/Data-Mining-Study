6e6a688af0c16258e735f909dfe013d12b15da26
Jensen-holm <holmj@mail.gvsu.edu>
2023-04-20 11:54:04 -0400

    added animated plot of loss history

--- neural_network/plot.py
+++ neural_network/plot.py
@@ -1,15 +1,22 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 sns.set()
 
 
-def loss_history_plt(loss_history: np.array) -> None:
-    sns.lineplot(
-        x=np.arange(len(loss_history)),
-        y=loss_history,
-    )
-    plt.xlabel("Epoch")
-    plt.ylabel("Training Loss")
+def loss_history_plt(loss_history: list) -> None:
+    fig, ax = plt.subplots()
+
+    def animate(i):
+        ax.clear()
+        sns.lineplot(
+            x=range(i),
+            y=loss_history[:i],
+            ax=ax,
+        )
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("Training Loss")
+
+    _ = FuncAnimation(fig, animate, frames=len(loss_history), interval=100)
     plt.show()
