a63e09b68015a324f23579fd14a7001950cb13fb
Jensen-holm <holmj@mail.gvsu.edu>
2023-04-20 22:04:45 -0400

    added baseline stuff for making this a flask app with tailwind

--- neural_network/activation.py
+++ neural_network/activation.py
@@ -4,7 +4,7 @@ relu = lambda x: np.maximum(x, 0)
 relu_prime = lambda x: np.where(x > 0, 1, 0)
 
 tanh = lambda x: np.tanh(x)
-tanh_prime = lambda x: 1 - tanh(x) ** 2
+tanh_prime = lambda x: 1 - np.tanh(x) ** 2
 
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
 sigmoid_prime = lambda x: sigmoid(x) / 1.0 - sigmoid(x)
