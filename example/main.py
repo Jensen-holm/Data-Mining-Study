31747ca70981fd8c158e1b0ab263d5607ff72d9a
Jensen-holm <holmj@mail.gvsu.edu>
2023-05-04 18:58:18 -0400

    python example for creating a plot of loss history over time for neural_network

---
+++ example/main.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+import json
+
+ENDPOINT: str = "https://data-mining-from-scratch-backend.onrender.com/"
+
+request_params = {
+    "algorithm": "neural-network",
+    "arguments": {
+        "epochs": 100,
+        "activation_func": "tanh",
+        "hidden_size": 8,
+        "learning_rate": 0.01
+    }
+}
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+r = requests.post(
+    ENDPOINT,
+    headers=headers,
+    data=json.dumps(request_params),
+)
+
+model = r.json()
+
+
+def plot():
+    sns.set()
+    plt.plot(model["loss_history"])
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Loss History")
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot()
