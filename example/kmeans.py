5e9f4d3e565a05e3bd81658e283e9724447ffaa6
Jensen-holm <holmj@mail.gvsu.edu>
2023-05-08 14:06:03 -0400

    kmeans with example plot done and ready for deployment

--- example/kmeans.py
+++ example/kmeans.py
@@ -1,5 +1,9 @@
-import requests
+
 import json
+import requests
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 ENDPOINT: str = "http://127.0.0.1:5000/"
 
@@ -7,7 +11,7 @@ request_params = {
     "algorithm": "kmeans-clustering",
     "arguments": {
         "k": 3,
-        "max_iter": 10,
+        "max_iter": 100,
     },
 }
 
@@ -23,5 +27,31 @@ r = requests.post(
 )
 
 
+def plot():
+    cluster_data = r.json()["clusters"]
+    # plot the clusters and data points
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    sns.set()
+    for cluster in cluster_data:
+        sns.scatterplot(
+            x=[point[0] for point in cluster["points"]],
+            y=[point[1] for point in cluster["points"]],
+            label=f"Cluster {cluster['cluster_id']}",
+            ax=ax,
+        )
+        ax.scatter(
+            x=cluster["centroid"][0],
+            y=cluster["centroid"][1],
+            marker="x",
+            s=100,
+            linewidth=2,
+            color="black"
+        )
+    ax.set_title("K-means Clustering")
+    ax.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    print(r.json())
+    plot()
